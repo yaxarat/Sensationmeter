784f0219d901c06dda136668cc1ebcc5c5e98e61
Yashar <ya.atajan@gmail.com>
2019-07-19 16:33:39 -0400

    all insert dao functions created

--- app/src/main/java/com/example/sensationmeter/database/repository/service/DrinkDao.kt
+++ app/src/main/java/com/example/sensationmeter/database/repository/service/DrinkDao.kt
@@ -3,10 +3,13 @@ package com.example.sensationmeter.database.repository.service
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.sensationmeter.database.entity.Drink
 
 @Dao
 interface DrinkDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(drink: Drink)
+
+//    @Query("SELECT * FROM drink_log")
 }
